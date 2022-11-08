0bed1f756176ac002af51ae67d171318b0df77d5
huylg99 <lehuy154@gmail.com>
2022-11-08 17:31:44 +0700

    Setlected dropdown  dont't need bloc

--- lib/models/more_service_model.dart
+++ lib/models/more_service_model.dart
@@ -1,91 +1,19 @@
 import 'package:equatable/equatable.dart';
+import 'package:hahaloloapp/models/accompanied_service_model.dart';
 
 class MoreServiceModel extends Equatable {
+  AccompaniedServiceData? accompaniedServiceData;
   String? idSelectedMenuItem;
-  String? id;
-  String? currency;
-  String? pt100;
-  String? pn100;
-  String? pp100;
-  String? pt250;
-  String? tv451;
-  int? tn452;
-  T250? t250;
-  DateTime? dl146;
-  String? dl147;
+  String? name;
+  num? qty;
   MoreServiceModel({
-    this.idSelectedMenuItem,
-    this.id = '',
-    this.pn100 = '',
-    this.dl147 = '',
-    this.dl146,
-    this.currency = '',
-    this.t250,
-    this.pp100 = '',
-    this.pt100 = '',
-    this.pt250 = '',
-    this.tn452 = 0,
-    this.tv451 = '',
+    String? idSelectedMenuItem,
+    AccompaniedServiceData? accompaniedServiceData,
+    String? id,
+    String? name,
+    num? qty,
   });
 
   @override
   List<Object?> get props => [];
-
-  MoreServiceModel copyWith({
-    String? idSelectedMenuItem,
-    String? currency,
-    String? pt100,
-    String? pn100,
-    String? pp100,
-    String? pt250,
-    String? tv451,
-    int? tn452,
-    String? id,
-    DateTime? dl146,
-    String? dl147,
-  }) {
-    return MoreServiceModel(
-      idSelectedMenuItem: idSelectedMenuItem ?? this.idSelectedMenuItem,
-      currency: currency ?? this.currency,
-      pt250: pt250 ?? this.pt250,
-      pt100: pt100 ?? this.pt100,
-      pn100: pn100 ?? this.pn100,
-      pp100: pp100 ?? this.pp100,
-      tn452: tn452 ?? this.tn452,
-      tv451: tv451 ?? this.tv451,
-      id: id ?? this.id,
-      dl146: dl146 ?? this.dl146,
-      dl147: dl147 ?? this.dl147,
-    );
-  }
-}
-
-class T250 {
-  T250({
-    this.t251,
-    this.tv253,
-    this.tv254,
-    this.pt150,
-    this.tv255,
-    this.tn256,
-    this.id,
-  });
-
-  T251? t251;
-  String? tv253;
-  String? tv254;
-  String? pt150;
-  String? tv255;
-  int? tn256;
-  String? id;
-}
-
-class T251 {
-  T251({
-    this.lang,
-    this.tv251,
-  });
-
-  String? lang;
-  String? tv251;
 }
--- lib/widget/book_tour_info_widget/accompained_service_view.dart
+++ lib/widget/book_tour_info_widget/accompained_service_view.dart
@@ -1,81 +1,127 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hahaloloapp/models/accompanied_service_model.dart';
+import 'package:hahaloloapp/models/more_service_model.dart';
 
-import '../../bloc/accompanied_service_bloc/accompanied_bloc.dart';
 import 'accompanied_service_widget.dart';
 
 class AccompaniedServiceView extends StatefulWidget {
-  const AccompaniedServiceView({Key? key}) : super(key: key);
-
+  AccompaniedServiceView({
+    Key? key,
+    required this.accompaniedList,
+  }) : super(key: key);
+  List<AccompaniedServiceData> accompaniedList;
   @override
   AccompaniedServiceViewState createState() => AccompaniedServiceViewState();
 }
 
 class AccompaniedServiceViewState extends State<AccompaniedServiceView> {
-  List<Widget> widgetList = [];
-
+  List<AccompaniedServiceData> accompaniedList = [];
+  List<AccompaniedServiceData> selectedList = [];
+  List<MoreServiceModel> moreServiceList = [];
+  bool check = true;
   @override
   void initState() {
     super.initState();
-    widgetList.add(AccompaniedService(
-      id: '1',
-    ));
-    context
-        .read<AccompaniedServiceBloc>()
-        .add(AccompaniedServiceCompareSelected());
+    moreServiceList = List.generate(1, (index) => MoreServiceModel());
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AccompaniedServiceBloc, AccompaniedServiceState>(
-        builder: (context, state) {
-      void deleteWidget(int i) {
-        setState(() {
-          widgetList.removeAt(i);
-        });
+    if (widget.accompaniedList.isNotEmpty) {
+      if (check == true) {
+        accompaniedList = widget.accompaniedList;
       }
+      setState(() {});
+      check = false;
+    }
 
-      return Column(
-        children: [
-          ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: widgetList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return AccompaniedService(
-                  id: '${state.selectedData}',
-                  onDeleteClicked: () {
-                    context
-                        .read<AccompaniedServiceBloc>()
-                        .add(AccompaniedServiceDeleted());
-                    deleteWidget(index);
-                  },
-                );
-              }),
-          widgetList.isNotEmpty &&
-                      state.listAccompaniedService.length > widgetList.length ||
-                  widgetList.isEmpty &&
-                      state.listAccompaniedService.length > widgetList.length
-              ? TextButton(
-                  onPressed: () {
-                    setState(() {
-                      widgetList.add(AccompaniedService(
-                        id: state.selectedData?.id,
-                      ));
-                    });
-                  },
-                  child: const Text(
-                    '+ Thêm dịch vụ',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      color: Colors.lightBlueAccent,
-                      fontSize: 20,
-                    ),
+    return Column(
+      children: [
+        ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: moreServiceList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return AccompaniedService(
+                onSelected: (value) {
+                  setState(() {});
+
+                  if (selectedList.isEmpty) {
+                    moreServiceList[index].name = value?.t250.t251.tv251;
+                    moreServiceList[index].idSelectedMenuItem = value?.id;
+                    selectedList.add(value!);
+                    accompaniedList = widget.accompaniedList
+                        .toSet()
+                        .difference(selectedList.toSet())
+                        .toList();
+                  }
+
+                  selectedList.removeWhere((element) =>
+                      element.id == moreServiceList[index].idSelectedMenuItem);
+
+                  selectedList.add(value!);
+
+                  accompaniedList = widget.accompaniedList
+                      .toSet()
+                      .difference(selectedList.toSet())
+                      .toList();
+                  moreServiceList[index].name = value.t250.t251.tv251;
+                  moreServiceList[index].idSelectedMenuItem = value.id;
+                },
+                accompaniedServiceData: accompaniedList,
+                name: moreServiceList[index].name,
+
+                /// Delete
+                onDeleted: (value) {
+                  setState(() {});
+                  if (moreServiceList[index].name == null) {
+                    moreServiceList.removeAt(index);
+                  } else if (moreServiceList[index].name != null &&
+                      moreServiceList[index].idSelectedMenuItem !=
+                          selectedList[index].id) {
+                    moreServiceList.removeWhere((element) =>
+                        element.idSelectedMenuItem == selectedList[index].id);
+
+                    selectedList.add(value!);
+                    // selectedList.remove(value!);
+
+                  } else if (moreServiceList[index].idSelectedMenuItem ==
+                      selectedList[index].id) {
+                    moreServiceList.removeWhere((element) =>
+                        element.idSelectedMenuItem == selectedList[index].id);
+                    selectedList.remove(value);
+                    accompaniedList = widget.accompaniedList
+                        .toSet()
+                        .difference(selectedList.toSet())
+                        .toList();
+                    print(accompaniedList);
+                  }
+                  accompaniedList = widget.accompaniedList
+                      .toSet()
+                      .difference(selectedList.toSet())
+                      .toList();
+                  print(accompaniedList);
+                },
+              );
+            }),
+        moreServiceList.length != widget.accompaniedList.length
+            ? TextButton(
+                onPressed: () {
+                  setState(() {
+                    moreServiceList.add(MoreServiceModel());
+                  });
+                },
+                child: const Text(
+                  '+ Thêm dịch vụ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    color: Colors.lightBlueAccent,
+                    fontSize: 20,
                   ),
-                )
-              : const SizedBox(),
-        ],
-      );
-    });
+                ),
+              )
+            : const SizedBox(),
+      ],
+    );
   }
 }
